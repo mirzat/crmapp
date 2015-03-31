4e6db46a7041387c7552fb31b16bb1e9180dd9a2
mirzat <tashfique.mirza@gmail.com>
2015-03-31 00:29:56 -0500

    Added codeception tests 1. $ cept bootstrap 2. $ cept generate:cept acceptance SmokeTest 3. Configured codeception modules.config.PhpBrowser.url token

---
+++ tests/acceptance/_steps/CRMOperatorSteps.php
@@ -0,0 +1,13 @@
+<?php
+namespace AcceptanceTester;
+
+class CRMOperatorSteps extends \AcceptanceTester
+{
+    public function y()
+    {
+        $I = $this;
+    }    public function yes()
+    {
+        $I = $this;
+    }
+}
